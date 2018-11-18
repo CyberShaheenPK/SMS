b4c7f42aa3a3adbd403e43400d3269f5a46290cf
sabirsiddique98 <45005751+sabirsiddique98@users.noreply.github.com>
2018-11-18 17:38:28 +0500

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author mma
+ */
+class Course {
+private String courseName;
+private String courseCode;
+private Class courseClass;
+public void addCourse()
+{
+   
+}
+public void removeCourse()
+{
+   
+}
+}
