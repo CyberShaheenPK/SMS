116f138fbb8e968557d1aba1f4dca5d33b20bfbb
sabirsiddique98 <45005751+sabirsiddique98@users.noreply.github.com>
2018-11-18 17:37:43 +0500

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+import java.util.ArrayList;
+/**
+ *
+ * @author mma
+ */
+public class Admin {
+private String name;
+private int password;
+private int id;
+ArrayList arrayCourse = new ArrayList(100);
+ArrayList arrayClass = new ArrayList(100);
+public void addTeacher()
+{
+   
+}
+public void addStudent()
+{
+   
+}
+public void removeTeacher ()
+{
+   
+}
+public void removeStudent()
+{
+   
+}
+public void addSchool()
+{
+   
+}
+public void addCourse(Course C)
+{
+ arrayCourse.add(C);   
+}
+public void addClass(Class C)
+{
+ arrayClass.add(C);   
+}
+}
